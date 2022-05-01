76a2bce01cdfc3a18236441ebb422765869e9b9c
HUMMER <zeftawyapps@gmail.com>
2022-05-01 11:49:45 +0200

    game

--- lib/ui/values.dart
+++ lib/ui/values.dart
@@ -18,13 +18,113 @@ LinearGradient(
 Color bcbeg = Color(0xFF7B0303);
 Color bcbeg3 = Color(0xff614c04);
 Color bcbeg2 = Color(0xff095304);
+Color bcbeg4 = Color(0xFF0648EE);
+Color bcbeg5 = Color(0xff4692ef);
+Color bcbeg6 = Color(0xff06f9af);
+Color bcbeg7 = Color(0xFF7CD005);
+Color bcbeg8 = Color(0xffcea602);
+Color bcbeg9 = Color(0xfff82038);
+Color bcbeg10 = Color(0xFFD68D8D);
+Color bcbeg11 = Color(0xffdcc165);
+Color bcbeg12 = Color(0xff7cee74);
+Color bcbeg13 = Color(0xFFF43AF1);
+Color bcbeg14 = Color(0xffa308f8);
+Color bcbeg15 = Color(0xfff70582);
+Color bcbeg16 = Color(0xFFF25517);
+Color bcbeg17 = Color(0xfff18838);
+Color bcbeg18 = Color(0xfff5c72b);
+Color bcbeg19 = Color(0xFF075AF6);
+Color bcbeg20 = Color(0xffb31d47);
+Color bcbeg21 = Color(0xff85f27e);
+Color bcbeg22 = Color(0xFFCFACE9);
+Color bcbeg23 = Color(0xffe2dac1);
+Color bcbeg24 = Color(0xff66e9e7);
+Color bcbeg25 = Color(0xFF796D61);
+Color bcbeg26 = Color(0xff43423e);
+Color bcbeg27 = Color(0xff80847f);
+Color bcbeg28 = Color(0xFFE5BCC6);
+Color bcbeg29 = Color(0xffbfabdc);
+Color bcbeg30 = Color(0xff76b5c0);
+Color bcbe31 = Color(0xFF290D5E);
+Color bcbeg32 = Color(0xff3b1056);
+
+Color bcbeg33 = Color(0xFF045971);
+Color bcbeg34 = Color(0xff1b9d57);
+Color bcbeg35 = Color(0xff2f4f2d);
+Color bcbeg36 = Color(0xFFA55656);
+Color bcbeg37 = Color(0xffa49769);
+Color bcbeg38 = Color(0xff437040);
+Color bcbeg39 = Color(0xFFE12C2C);
+Color bcbeg40 = Color(0xffe4b71c);
+Color bcbeg41 = Color(0xff2d9626);
+Color bcbeg42 = Color(0xFFF402BF);
+Color bcbeg43 = Color(0xffefc636);
+Color bcbeg44 = Color(0xff427af2);
+Color bcbeg45 = Color(0xff93df8e);
+
+
+
+
+
+
+
+
 
 Color bcbegcard = Color(0xffa68309);
-Color bcbegcard1 = Color(0xffefe8d6);
+Color bcbegcard1 = Color(0xff0911a6);
+Color bcbegcard2 = Color(0xff246807);
+Color bcbegcard3 = Color(0xff5f1706);
+Color bcbegcard4 = Color(0xff5e045b);
+Color bcbegcard5 = Color(0xff5f030f);
+Color bcbegcard6 = Color(0xff3d0767);
+Color bcbegcard7 = Color(0xff0a1040);
+Color bcbegcard8 = Color(0xff16492e);
+Color bcbegcard9 = Color(0xff2a3030);
+Color bcbegcard10 = Color(0xff444441);
+Color bcbegcard11 = Color(0xff2d024a);
+Color bcbegcard12 = Color(0xff4f0307);
+
+Color bcbegcardf1 = Color(0xffefe8d6);
+Color bcbegcardf2 = Color(0xfff1ce75);
+Color bcbegcardf3 = Color(0xfface36f);
+Color bcbegcardf4 = Color(0xffefddad);
+Color bcbegcardf5 = Color(0xffee81c2);
+Color bcbegcardf6 = Color(0xfff0a1a7);
+Color bcbegcardf7 = Color(0xff8c8080);
+Color bcbegcardf8 = Color(0xffc4d1c6);
+Color bcbegcardf9 = Color(0xff87a0a5);
+Color bcbegcardf10 = Color(0xff9a8ac3);
+Color bcbegcardf11 = Color(0xffc4b0bf);
 
 
 Color bcbegGamebtn = Color(0xff8ca609);
+Color bcbegGamebtn1 = Color(0xff35a609);
+Color bcbegGamebtn2 = Color(0xff0945a6);
+Color bcbegGamebtn3 = Color(0xff9c09a6);
+Color bcbegGamebtn4 = Color(0xffa6090c);
+Color bcbegGamebtn5 = Color(0xffa62809);
+Color bcbegGamebtn6 = Color(0xff0994a6);
+Color bcbegGamebtn7 = Color(0xff09a648);
+Color bcbegGamebtn8 = Color(0xffa68109);
+Color bcbegGamebtn9 = Color(0xff09a60e);
+
+class Theams {
+ var  backgroungrideGame ;
+ var cardBackForwardBackGround ;
+ var cardFrontBackGround ;
+ var WindowBackGround ;
+ var HelpCardBackGround ;
+
+}
 
 
+class GameBackGround{
+
+ late  List<LinearGradient> BackGounds =[
+
+ ];
+ late  List<int> Choosed ;
+
+}
 
 
